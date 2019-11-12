c811095dd87ebaee00339188929a8359754cc7f2
kenbo736 <kenbo736@tlhw-4-1.ad.liu.se>
2019-11-12 17:14:07 +0100

    working on it

---
+++ Lab_1/up_1/temp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+  int c{};
+  double k{c + 273.15};
+  double f{(c * 1.8) + 32 };
+  double r{c * 0.8};
+
+  cout << "Ange startvärde: " << endl;
+  cin >> c;
+  cout << "Ange slutvärde: " << endl;
+  cin >>
+}
+
+void tabell(){
+
+}
