57ee5b11236f69e2bad2ec2a2c7b5d37d7a94287
Kenneth <kenbo736@student.liu.se>
2019-11-19 08:10:24 +0100

    done

--- Lab_1/up_1/temp.cpp
+++ Lab_1/up_1/temp.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
+void tabell(int c, double k, double f, double r, int start, int end);
 
 int main(){
   int c{};
-  double k{c + 273.15};
-  double f{(c * 1.8) + 32 };
-  double r{c * 0.8};
+  double k{};
+  double f{};
+  double r{};
+  int start{};
+  int end{};
 
   cout << "Ange startvärde: " << endl;
-  cin >> c;
+  cin >> start;
   cout << "Ange slutvärde: " << endl;
-  cin >>
+  cin >> end;
+  while(start > end)
+  { 
+    cout << "Felaktigt slutvärde! " << endl;
+    cout << "Ange slutvärde: " << endl;
+    cin >> end;
+  }
+
+  tabell(c, k, f, r, start, end);
+  return 0;
 }
 
-void tabell(){
-
+void tabell(int c, double k, double f, double r, int start, int end)
+{
+  cout << "Celsius     Kelvin  Farenheit   Réaumur" << endl;
+  cout << "---------------------------------------" << endl;
+  c = start;
+  while(start < end){
+    c = c+1;
+    k = c + 273.15;
+    f = (c * 1.8) + 32;
+    r = c * 0.8;
+    cout << fixed << setw(7) << c << setw(11) << setprecision(2) << k << setw(11) << setprecision(2) << f << setw(10) << setprecision(2) << r << endl;
+    start++;
+  }
 }
